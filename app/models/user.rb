6a56d4526f98e5c095329f5ef4520e9ad7b79ab0
kajihirocpa <syorinhiroki@gmail.com>
2021-08-08 17:13:30 +0900

    userモデルのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name,               presence: true
-  validates :first_name,            presence: true
-  validates :family_name,         presence: true
-  validates :read_first,           presence: true
-  validates :read_family,           presence: true
+  validates :first_name,            presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name,         presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :read_first,           presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :read_family,           presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day,           presence: true
+
+  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
 
   has_many :items
   has_many :orders
